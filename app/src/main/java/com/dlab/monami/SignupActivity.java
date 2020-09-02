327207847e4b44b11bbe015aa17bcd1d1c3d7bcf
youngjae <elite1111cyj@gmail.com>
2020-09-02 10:29:22 +0900

    error change

--- app/src/main/java/com/dlab/monami/SignupActivity.java
+++ app/src/main/java/com/dlab/monami/SignupActivity.java
@@ -6,7 +6,6 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
-import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -20,6 +19,7 @@ import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
@@ -30,18 +30,20 @@ import java.util.HashMap;
 public class SignupActivity extends AppCompatActivity {
     private DatabaseReference mPostReference;
     String uid="", pw="";
-    EditText usernameET,passwordET;
 
     ArrayList<String> data;
     ArrayAdapter<String> arrayAdapter;
 
     TextView signupbtn;
 
+    String TAG="signupActivity";
+
     // google -------------------------------------------------------
     public static final String GOOGLE_ACCOUNT = "google_account";
     private TextView nameET, emailET, passwordET, passwordconfirmET;
     private GoogleSignInAccount mGoogleSignInAccount;
     private GoogleSignInClient mGoogleSignInClient;
+    private FirebaseAuth firebaseAuth;
 
     // -------------------------------------------------------
 
@@ -55,6 +57,11 @@ public class SignupActivity extends AppCompatActivity {
         // Build a GoogleSignInClient with the options specified by gso.
         mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
 
+        //파이어베이스 접근 설정
+        // user = firebaseAuth.getCurrentUser();
+        firebaseAuth =  FirebaseAuth.getInstance();
+        //firebaseDatabase = FirebaseDatabase.getInstance().getReference();
+
         nameET = findViewById(R.id.username);
         emailET = findViewById(R.id.emailET);
         passwordET = findViewById(R.id.passwordET);
@@ -62,13 +69,12 @@ public class SignupActivity extends AppCompatActivity {
 
         mGoogleSignInAccount = getIntent().getParcelableExtra(GOOGLE_ACCOUNT);
 
-        setDataOnView();
+        //setDataOnView();
 
         signupbtn = findViewById(R.id.singupBtn);
         signupbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                final String name = nameET.getText().toString().trim();
                 final String email = emailET.getText().toString().trim();
                 String pwd = passwordET.getText().toString().trim();
                 String pwdcheck = passwordconfirmET.getText().toString().trim();
@@ -76,23 +82,23 @@ public class SignupActivity extends AppCompatActivity {
                 // password check
                 if(pwd.equals(pwdcheck)) {
                     Log.d(TAG, "등록 버튼 " + email + " , " + pwd);
-                    final ProgressDialog mDialog = new ProgressDialog(RegisterActivity.this);
+                    final ProgressDialog mDialog = new ProgressDialog(SignupActivity.this);
                     mDialog.setMessage("가입중입니다...");
                     mDialog.show();
 
                     //파이어베이스에 신규계정 등록하기
-                    firebaseAuth.createUserWithEmailAndPassword(email, pwd).addOnCompleteListener(RegisterActivity.this, new OnCompleteListener<AuthResult>() {
+                    firebaseAuth.createUserWithEmailAndPassword(email, pwd).addOnCompleteListener(SignupActivity.this, new OnCompleteListener<AuthResult>() {
                         @Override
                         public void onComplete(@NonNull Task<AuthResult> task) {
-
                             //가입 성공시
+                            Log.d(TAG, task.toString());
                             if (task.isSuccessful()) {
                                 mDialog.dismiss();
 
                                 FirebaseUser user = firebaseAuth.getCurrentUser();
                                 String email = user.getEmail();
                                 String uid = user.getUid();
-                                String name = mName.getText().toString().trim();
+                                String name = nameET.getText().toString().trim();
 
                                 //해쉬맵 테이블을 파이어베이스 데이터베이스에 저장
                                 HashMap<Object,String> hashMap = new HashMap<>();
@@ -105,27 +111,22 @@ public class SignupActivity extends AppCompatActivity {
                                 DatabaseReference reference = database.getReference("Users");
                                 reference.child(uid).setValue(hashMap);
 
-
                                 //가입이 이루어져을시 가입 화면을 빠져나감.
-                                Intent intent = new Intent(RegisterActivity.this, MainActivity.class);
+                                Intent intent = new Intent(SignupActivity.this, MainActivity.class);
                                 startActivity(intent);
                                 finish();
-                                Toast.makeText(RegisterActivity.this, "회원가입에 성공하셨습니다.", Toast.LENGTH_SHORT).show();
-
+                                Toast.makeText(SignupActivity.this, "회원가입에 성공하셨습니다.", Toast.LENGTH_SHORT).show();
                             } else {
                                 mDialog.dismiss();
-                                Toast.makeText(RegisterActivity.this, "이미 존재하는 아이디 입니다.", Toast.LENGTH_SHORT).show();
+                                Toast.makeText(SignupActivity.this, "이미 존재하는 아이디 입니다.", Toast.LENGTH_SHORT).show();
                                 return;  //해당 메소드 진행을 멈추고 빠져나감.
-
                             }
 
                         }
                     });
-
                     //비밀번호 오류시
                 }else{
-
-                    Toast.makeText(RegisterActivity.this, "비밀번호가 틀렸습니다. 다시 입력해 주세요.", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(SignupActivity.this, "비밀번호가 일치하지 않습니다. 다시 입력해 주세요.", Toast.LENGTH_SHORT).show();
                     return;
                 }
             }
@@ -134,10 +135,10 @@ public class SignupActivity extends AppCompatActivity {
 
     }
 
+    /*
     private void setDataOnView() {
         // implementation 'com.squareup.picasso:picasso:2.71828'
-
         profileName.setText(mGoogleSignInAccount.getDisplayName());
         profileEmail.setText(mGoogleSignInAccount.getEmail());
-    }
+    }*/
 }
